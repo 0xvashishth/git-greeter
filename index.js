beed37e84ad131189ee9dcff15b3cf04be427d1b
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-03-03 22:12:45 +0530

    all neccessary github bot events added according to the controllers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var issueController = require("./events/issue");
 var commentController = require("./events/comment");
-
+var pull_requestController = require("./events/pull_request");
 
 module.exports = (app, { getRouter }) => {
   // Checking, app is currently working or not
@@ -20,7 +21,41 @@ module.exports = (app, { getRouter }) => {
     console.log("Inside any things");
   });
 
+  // on opening the issue
+  app.on("issues.opened", async (context) => {
+    await issueController.issueCreate(context);
+    return await issueController.issueAddLabel(context)
+  });
+  // on editing the issue
+  app.on("issues.edited", async (context) => {
+    return await issueController.issueEdit(context);
+  });
+  // on closing the issue
+  app.on("issues.closed", async (context) => {
+    return await issueController.issueClose(context);
+  })
+  // on reopening the issue
+  app.on("issues.reopened", async (context) => {
+    return await issueController.issueReopened(context);
+  })
 
+  // on opening the pull request
+  app.on("pull_request.opened", async (context) => {
+    await pull_requestController.pull_requestCreate(context);
+    return await pull_requestController.pull_requestAddLabel(context)
+  });
+  // on synchronizing the pull request
+  app.on("pull_request.synchronize", async (context) => {
+    return await pull_requestController.pull_requestAddLabelOnSynchronize(context)
+  });
+  // on cosing or merging the pull request
+  app.on("pull_request.closed", async (context) => {
+    return await pull_requestController.pull_requestClose(context);
+  });
+  // on reopening the pull request
+  app.on("pull_request.reopened", async (context) => {
+    return await pull_requestController.pull_requestReopened(context);
+  });
 
   // on creating issue comments
   app.on("issue_comment.created", async (context) => {
